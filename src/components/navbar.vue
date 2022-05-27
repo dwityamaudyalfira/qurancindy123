d808f493a88c1fb0349c831a7e978e086509bca6
dwityamaudyalfira <76533748+dwityamaudyalfira@users.noreply.github.com>
2022-05-28 00:30:45 +0700

    Initial commit 123

---
+++ src/components/navbar.vue
@@ -0,0 +1,57 @@
+<template>
+  <nav class="navbar navbar-expand-lg " >
+    <div class="container-fluid">
+      <img  src="../assets/logoquran.png" class="rounded float-end"  width = 100 height = 100/>
+
+      <div class ="judul justify-content-center">Al-Qur'an web for you, enjoy!</div>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ml-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="/"></a>
+          </li>
+        </ul>
+
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "navbar"
+};
+</script>
+
+<style scoped>
+body {
+  padding: 0;
+  margin: 0;
+  background: #f2f6e9;
+}
+.navbar {
+  background: #166167;
+}
+.nav-link,
+.navbar-brand {
+  color: #fff;
+  cursor: pointer;
+}
+.nav-link {
+  margin-right: 1em !important;
+}
+.nav-link:hover {
+  color: #c7a9a9;
+}
+.navbar-collapse {
+  justify-content: flex-end;
+}
+.judul {
+  text-align: center;
+  color: #cbb6b6;
+  font-size: 20px;
+  font-family: "Book Antiqua";
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,26 @@
+<script setup>
+import Navbar from "../components/navbar.vue";
+import { onMounted } from "vue";
+
+onMounted(() => {
+  document.getElementsByTagName("body")[0].style.backgroundColor ="#56c5d0"
+  document.getElementsByTagName("body")[0].style.backgroundSize ="cover";
+});
+</script>
+
+<template>
+  <main>
+    <navbar/>
+    <div class = "title">
+      <h1> "Bacalah Al-Qur'an mu, agar selamat kehidupan dunia dan akhiratmu." </h1>
+    </div>
+
+  </main>
+</template>
+
+<style>
+.title{
+  text-align: center;
+  font-family: -apple-system;
+}
+</style>
